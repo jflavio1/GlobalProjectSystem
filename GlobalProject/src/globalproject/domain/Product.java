56d719a82464d184e083c4564b476c8c71ca5872
JoseFlavio <jflavio90@gmail.com>
2018-10-20 20:06:30 -0500

    add product entity

--- GlobalProject/src/globalproject/domain/Product.java
+++ GlobalProject/src/globalproject/domain/Product.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package globalproject.domain;
 
 /**
  *
- * @author joseflavio
+ * @author Jose Flavio
  */
 public class Product {
+    
+    private int prodId;
+    private String prodCode;
+    private String prodName;
+    private double prodPrice;
+
+    public Product(int prodId, String prodCode, String prodName, double prodPrice) {
+        this.prodId = prodId;
+        this.prodCode = prodCode;
+        this.prodName = prodName;
+        this.prodPrice = prodPrice;
+    }
+
+    public int getProdId() {
+        return prodId;
+    }
+    
+    public String getProdCode() {
+        return prodCode;
+    }
+
+    public void setProdCode(String prodCode) {
+        this.prodCode = prodCode;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public double getProdPrice() {
+        return prodPrice;
+    }
+
+    public void setProdPrice(double prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+    
+    
     
 }
