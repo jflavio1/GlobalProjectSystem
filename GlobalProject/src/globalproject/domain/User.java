2b8d402fbfc89b127cfe07567490e25153011c20
JoseFlavio <jflavio90@gmail.com>
2018-10-20 19:17:25 -0500

    add user class methods and attrs

--- GlobalProject/src/globalproject/domain/User.java
+++ GlobalProject/src/globalproject/domain/User.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package globalproject.domain;
 
 /**
  *
- * @author joseflavio
+ * @author Jose Flavio
  */
 public class User {
+    
+    private String userFormalName;
+    private String userName;
+    private String userPassword;
+
+    public User(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    public String getUserFormalName() {
+        return userFormalName;
+    }
+
+    public void setUserFormalName(String userFormalName) {
+        this.userFormalName = userFormalName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
     
 }
