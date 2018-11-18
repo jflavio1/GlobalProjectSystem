eed66874482674722bf5cbfa7ae1ea3508f7effd
JoseFlavio <jflavio90@gmail.com>
2018-11-17 19:46:33 -0500

    added de, fr

--- GlobalProject/src/globalproject/GlobalProject.java
+++ GlobalProject/src/globalproject/GlobalProject.java
@@ -1,6 +1,11 @@
 package globalproject;
 
 import globalproject.ui.Welcome;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -8,7 +13,18 @@ import globalproject.ui.Welcome;
  */
 public class GlobalProject {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
+        if(args.length > 0){
+//            File file = new File(args[0] + ".properties");
+//            PropertiesConfiguration config = new PropertiesConfiguration();
+//            PropertiesConfigurationLayout layout = new PropertiesConfigurationLayout(config);
+//            layout.load(new InputStreamReader(new FileInputStream(file)));
+//
+//            config.setProperty("test", "testValue");
+//            layout.save(new FileWriter("path\\to\\properties\\file.properties", false));
+        }
+        
         Welcome welcomeWindows = new Welcome();
         welcomeWindows.setVisible(true);
     }
